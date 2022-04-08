dcf396be2a5559bea894ecb0002afc9f3db11c0b
KhairienNissa <cacakhairien@gmail.com>
2022-04-08 14:58:59 +0700

    project web

---
+++ connection/db.js
@@ -0,0 +1,34 @@
+// const {Pool} = require('pg')
+
+// const dbPool = new Pool({
+//     database: 'project_web',
+//     port: 5432,
+//     user: 'postgres',
+//     password: 'khairien'
+// })
+
+// module.exports = dbPool
+
+const { Pool } = require('pg')
+
+const isProduction = process.env.NODE_ENV === "production";
+let dbPool
+
+if (isProduction) {
+    dbPool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
+} else {
+
+    dbPool = new Pool({
+        database: 'project_web',
+        port: 5432,
+        user: 'postgres',
+        password: 'khairien'
+    })
+
+}
+module.exports = dbPool
